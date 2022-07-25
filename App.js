037ae02585a10526218cd3e1c2393ace260da42b
avaneehirapara <avnihirapara0706@gmail.com>
2022-07-25 11:38:34 +0530

    city

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Calcy from './app/Calcy'
+import City from './app/City'
 
 export default function App() {
  return (
     <View>
     <Calcy />
+    <City />
     </View>
   )
 }
